a8cdb50f3e14406f2c8715d2782c40b7596dbb32
Luiz Felipe <luizfelipecastrovb@gmail.com>
2023-11-27 16:46:41 -0300

    Added banner area

---
+++ src/components/Banner/index.js
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function Banner() {
+  return (
+    <header className="banner">
+      <img src="/images/banner.png" alt="banner" />
+    </header>
+  );
+}
+
+export default Banner;
