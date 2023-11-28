aa7f307966d8b0329ffee6d9833cf77f6e24e944
Luiz Felipe <luizfelipecastrovb@gmail.com>
2023-11-28 15:01:47 -0300

    Added team component

---
+++ src/components/Team/index.js
@@ -0,0 +1,28 @@
+import Colaborator from "../Colaborator";
+import "./index.css";
+
+const Team = (props) => {
+  return (
+    props.colaborators.length > 0 && (
+      <section
+        className="team"
+        style={{ backgroundColor: props.secondaryColor }}
+      >
+        <h3 style={{ borderColor: props.primaryColor }}>{props.name}</h3>
+        <div className="colaborators">
+          {props.colaborators.map((colaborator) => (
+            <Colaborator
+              key={colaborator.name}
+              name={colaborator.name}
+              job={colaborator.job}
+              image={colaborator.image}
+              backgroundColor={props.primaryColor}
+            />
+          ))}
+        </div>
+      </section>
+    )
+  );
+};
+
+export default Team;
