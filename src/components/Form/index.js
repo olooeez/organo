e1da5cf0b1483f8d882183f17fca74d3bd102a15
Luiz Felipe <luizfelipecastrovb@gmail.com>
2023-11-30 13:48:46 -0300

    Added team creation

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from "uuid";
+import { darken } from "polished";
 import { useState } from "react";
 import Button from "../Button";
 import Input from "../Input";
@@ -9,10 +11,13 @@ const Form = (props) => {
   const [job, setJob] = useState("");
   const [image, setImage] = useState("");
   const [team, setTeam] = useState("");
+  const [teamName, setTeamName] = useState("");
+  const [teamColor, setTeamColor] = useState("");
 
   const onSave = (event) => {
     event.preventDefault();
     props.onColaboratorSigned({
+      id: uuidv4(),
       name: name,
       job: job,
       image: image,
@@ -24,6 +29,19 @@ const Form = (props) => {
     setImage("");
     setTeam("");
   };
+
+  const onTeamCreate = (event) => {
+    event.preventDefault();
+    props.onTeamSigned({
+      id: uuidv4(),
+      name: teamName,
+      primaryColor: teamColor,
+      secondaryColor: darken(0.1, teamColor),
+    })
+
+    setTeamName("");
+    setTeamColor("");
+  }
 
   return (
     <section className="form">
@@ -58,6 +76,25 @@ const Form = (props) => {
           value={team}
         />
         <Button>Criar Card</Button>
+      </form>
+      <form onSubmit={onTeamCreate}>
+        <h2>Preencha os dados para criar o card do colaborador</h2>
+        <Input
+          onChange={(value) => setTeamName(value)}
+          required={true}
+          label="Nome"
+          placeholder="Digite o nome do time"
+          value={teamName}
+        />
+        <Input
+          onChange={(value) => setTeamColor(value)}
+          required={true}
+          label="Cargo"
+          placeholder="Digite a cor do time"
+          value={teamColor}
+          type="color"
+        />
+        <Button>Criar Time</Button>
       </form>
     </section>
   );
